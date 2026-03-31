c621798c16261025bb8ac4d2b14353da5b1622fe
Brawnyh <mamadouleye2b@gmail.com>
2026-03-31 23:36:56 +0200

    pesadilla

--- TPs/TP2/Code/main.c
+++ TPs/TP2/Code/main.c
@@ -17,29 +17,68 @@
 void print_token(const void* e, void* user_param);
 void print_queue(FILE* f, Queue* q);
 
+
+bool isSymbol(char c){
+	if (c=='+' || c== '-' || c=='/' || c=='*' || c== '^' || c== ')' || c== '('){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+Queue *stringToTokenQueue(const char * expression){
+	Queue* q=create_queue();
+	const char*curpos=expression;
+	//int i=0;
+	//int tmpIndex=0;
+	//char tmp[128];
+	while(*curpos!='\0'){
+		while (*curpos == ' ' || *curpos =='\n'){
+			curpos++;
+		}
+		if (*curpos=='\0') break;
+
+		if (isSymbol(*curpos)){
+			Token *tok=create_token_from_string(curpos,1);
+			queue_push(q,tok);
+			curpos++;
+			
+		}else{
+			const char *debut=curpos;
+			int longueur=0;
+			while(*curpos !='\0' && !isSymbol(*curpos) && *curpos != ' ' && *curpos != '\n'){
+				curpos++;
+				longueur++;
+			}
+			Token *tok =create_token_from_string(debut,longueur);
+			queue_push(q,tok);
+		}
+		
+
+	}
+
+	return q;
+}
+
+
 /** 
  * Function to be written by students
  */
 void computeExpressions(FILE* input) {
 	(void)input;
-	//hay que terminar esto
-	//input=fopen((char *)input,"r"); tal vez no e snecesario abrirlo aqui
-	//if (input==0){perror("erreur d'ouverture");exit(1);}
-	char *line=(char*)malloc(sizeof(char));
-	if (line==NULL){perror("erreur d'allocation");exit(1);}
-	char buffer[1024];
-	//on stock le nb de ligne du fichier(exemple dans man)
-	//ssize_t nlus;
-	size_t longeur;
-	ssize_t nlus=getline(&line,&longeur,input);
-	while (nlus!=0){
-		fread(buffer,longeur,1,input);
-		printf("Input: %s\n",buffer);
-		nlus--;
+	char *line=NULL;
+	size_t longeur=0;
+	ssize_t nlus;
+	//Queue *q;
+	while((nlus=getline(&line,&longeur,input))!=-1){
+		//printf("Input: %s\n",nlus);
+		if (nlus>1){
+			printf("Input: %s",line);
+			printf("Infix: ");
+			print_queue(input,stringToTokenQueue(line));
+		}
+		
 	}
-	
-	
-
 	free(line);
 	//fclose(input);
 }
@@ -81,3 +120,5 @@ void print_queue(FILE* f, Queue* q) {
 	fprintf(f, "(%d) --  ", queue_size(q));
 	queue_map(q, print_token, f);
 }
+
+
