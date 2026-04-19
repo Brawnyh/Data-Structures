25212cc36f0857bbf12e2de12e67f0e3f102cb0d
Brawnyh <mamadouleye2b@gmail.com>
2026-04-19 14:19:00 +0200

    mergesort

--- TPs/TP3/Code/list.c
+++ TPs/TP3/Code/list.c
@@ -26,6 +26,11 @@ struct s_List {
 	int size;
 };
 
+typedef struct {
+    LinkedElement* tete;  
+    LinkedElement* queue;  
+} SubList;
+
 
 /*-----------------------------------------------------------------*/
 
@@ -182,10 +187,99 @@ List* list_map(List* l, ListFunctor f, void* environment) {
 	return l;
 }
 
+
+/*--------------------------------------------------------------------*/
+
+SubList list_split(SubList l) {
+    LinkedElement* l1 = l.tete;
+    LinkedElement* l2 = l.tete;
+
+    while (l2 != l.queue && l2->next != l.queue) {
+        l1 = l1->next;
+        l2 = l2->next->next;
+    }
+
+    SubList milieu;
+    milieu.queue = l1;
+    milieu.tete = l1->next;
+    l1->next->previous = NULL;
+    l1->next = NULL;
+    return milieu;
+}
+ 
+
+SubList list_merge(SubList gauche, SubList droite, OrderFunctor f) {
+    SubList resultat;
+    LinkedElement* courant;
+
+    if (f(gauche.tete->value, droite.tete->value)) {
+        resultat.tete = gauche.tete;
+        courant = gauche.tete;
+        gauche.tete = gauche.tete->next;
+    } else {
+        resultat.tete = droite.tete;
+        courant = droite.tete;
+        droite.tete = droite.tete->next;
+    }
+
+    while (gauche.tete != NULL && droite.tete != NULL) {
+        if (f(gauche.tete->value, droite.tete->value)) {
+            courant->next = gauche.tete;
+            gauche.tete->previous = courant;
+            courant = gauche.tete;
+            gauche.tete = gauche.tete->next;
+        } else {
+            courant->next = droite.tete;
+            droite.tete->previous = courant;
+            courant = droite.tete;
+            droite.tete = droite.tete->next;
+        }
+    }
+
+    if (gauche.tete != NULL) {
+        courant->next = gauche.tete;
+        gauche.tete->previous = courant;
+        resultat.queue = gauche.queue;
+    } else {
+        courant->next = droite.tete;
+        droite.tete->previous = courant;
+        resultat.queue = droite.queue;
+    }
+
+    return resultat;
+}
+
+
+SubList list_mergesort(SubList l, OrderFunctor f) {
+    if (l.tete == NULL || l.tete == l.queue)
+        return l;
+
+    SubList milieu = list_split(l);
+    SubList gauche = { l.tete, milieu.queue };
+    SubList droite = { milieu.tete, l.queue };
+    gauche = list_mergesort(gauche, f);
+    droite = list_mergesort(droite, f);
+    return list_merge(gauche, droite, f);
+}
+
+
 /*-----------------------------------------------------------------*/
 
 List* list_sort(List* l, OrderFunctor f) {
 	(void)f;
+	SubList sl;
+    sl.tete = l->sentinel->next;
+    sl.queue = l->sentinel->previous;
+
+    sl.tete->previous = NULL;
+    sl.queue->next = NULL;
+
+    sl = list_mergesort(sl, f);
+
+    l->sentinel->next = sl.tete;
+    sl.tete->previous = l->sentinel;
+    l->sentinel->previous = sl.queue;
+    sl.queue->next = l->sentinel;
 	return l;
 }
 
